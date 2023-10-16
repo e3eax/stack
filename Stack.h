768b9d2ff93a7c828f45c01cb3064bcf1f5e8b38
troublemanfx <143964705+troublemanfx@users.noreply.github.com>
2023-10-15 23:59:44 -0400

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -13,45 +13,44 @@ using std::vector;
 template <typename T>
 class Stack {
  private:
-  int size = 0;
+  int s_size = 0;
   int top = -1;
   vector<T> _vector = {};
 
-  void set_values() {
-    set_size();
-    set_top();
+ public:
+  Stack(const vector<T>& _v = {}) : _vector(std::move(_v)) {
+    s_size = _vector.size();
+    top = s_size - 1;
   }
 
- public:
-  Stack(const vector<T>& _v = {}) : _vector(std::move(_v)) { set_values(); }
+  int get_size() const { return s_size; }
 
-  void set_size() { size = size(); }
+  int get_top() const { return top; }
 
-  void set_top() { top = size() - 1; }
-
-  int size() const { return _vector.size(); }
-
-  int top() const { return top; }
-
-  bool is_empty() const { return get_size() <= 0; }
+  bool is_empty() const { return s_size <= 0; }
 
   void pop() {
-    if (size <= 0) return;
+    if (s_size <= 0) return;
 
-    _vector.erase(_vector.begin() + top);
-    set_values();
+    --top;
+    --s_size;
   }
 
   void push(T val) {
-    _vector.push_back(val);
-    set_values();
+    ++top;
+    ++s_size;
+    if (s_size > _vector.size()) {
+      _vector.push_back(val);
+    } else {
+      _vector[top] = val;
+    }
   }
 
   void change(int idx, T val) {
     if (is_empty()) {
       throw out_of_range("[change] -> Stack is empty");
     }
-    if (idx < 0 || idx >= size) {
+    if (idx < 0 || idx >= s_size) {
       throw out_of_range("[change] -> Index is out of range");
     }
     _vector.at(idx) = val;
@@ -61,7 +60,7 @@ class Stack {
     if (is_empty()) {
       throw out_of_range("[peek] -> Stack is empty");
     }
-    if (idx >= size || idx < 0) {
+    if (idx >= s_size || idx < 0) {
       throw out_of_range("[peek] -> Index is out of range");
     }
     return _vector.at(idx);
@@ -69,7 +68,7 @@ class Stack {
 
   void print_vals() {
     cout << endl;
-    cout << "Size: " << size << endl << endl;
+    cout << "Stack size: " << s_size << endl << endl;
     cout << "Top: " << top << endl;
   }
 
